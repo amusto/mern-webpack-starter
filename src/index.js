88e34cabb6abd9451774e5b35ca18f6f933540b8
Armando Musto <armando.musto@gmail.com>
2018-01-07 12:07:21 -0500

    Added built dist

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
     let fullname = 'Armando Musto';
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = `Welcome to the site, ${fullname}`;
+    element.innerHTML = `Welcome to the site, ${fullname}!!!`;
 
     return element;
 }
